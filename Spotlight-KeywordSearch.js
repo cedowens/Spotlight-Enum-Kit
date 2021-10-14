b62a6eef0eba41db75023a8315f8b53fc09ba362
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2021-10-14 12:13:13 -0400

    Update Spotlight-KeywordSearch.js

--- Spotlight-KeywordSearch.js
+++ Spotlight-KeywordSearch.js
@@ -10,14 +10,18 @@ var username = $.NSUserName().js;
 var home = "/Users/" + username
 
 //---------Check for Files With Keyword Password In User's Home Dir/Subdirs------------------
-results += "##################Password Keyword Search (from Spotlight db)############################\n";
+results += "\n##################Password Keyword Search (from Spotlight db)############################\n";
 
 var dir_check = ObjC.deepUnwrap(currentApp.doShellScript("mdfind password -onlyin " + home));
 var dir_check2 = dir_check.split('\r');
+var c1 = 0;
 for(let i=0; i<dir_check2.length; i++){
-	if (!(dir_check2[i].includes("/Application Support/")) && !(dir_check2[i].includes("/Library"))){
-		results += dir_check2[i];
-        	results += '\n';
+	if (!(dir_check2[i].includes("/Application Support/")) && !(dir_check2[i].includes("/Library")) && !(dir_check2[i].includes("/zoom")) ){
+		c1 = c1 + 1;
+		if (c1 <= 25){
+			results += dir_check2[i];
+                	results += '\n';
+		}
 	}
 
 }
@@ -27,10 +31,14 @@ results += "\n\n##################Token Keyword Search (from Spotlight db)######
 
 var d_check = ObjC.deepUnwrap(currentApp.doShellScript("mdfind token -onlyin " + home));
 var d_check2 = d_check.split('\r');
+var c2 = 0;
 for(let i=0; i<d_check2.length; i++){
-	if (!(d_check2[i].includes("/Application")) && !(d_check2[i].includes("Containers/")) ){
-                results += d_check2[i];
-                results += '\n';
+	if (!(d_check2[i].includes("/Application")) && !(d_check2[i].includes("Containers/")) && !(d_check2[i].includes("/zoom")) ){
+		c2 = c2 + 1;
+		if (c2 <= 25) {
+			results += d_check2[i];
+                	results += '\n';
+		}
 
 
         }
@@ -42,10 +50,14 @@ results += "\n\n##################Secret Keyword Search (from Spotlight db)#####
 
 var d2_check = ObjC.deepUnwrap(currentApp.doShellScript("mdfind secret -onlyin " + home));
 var d2_check2 = d2_check.split('\r');
+var c3 = 0;
 for(let i=0; i<d2_check2.length; i++){
-	if (!(d2_check2[i].includes("/Application Support/"))){
-		results += d2_check2[i];
-        	results += '\n';
+	if (!(d2_check2[i].includes("/Application Support/")) && !(d2_check2[i].includes("/zooom")) ){
+		c3 = c3 + 1;
+		if (c3 <= 25){
+			results += d2_check2[i];
+                	results += '\n';
+		}
 
 
 	}
@@ -57,3 +69,4 @@ return results
 }
 
 //Check()
+
