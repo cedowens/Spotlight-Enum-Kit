4b92eca99c04b74039091fe9cd9d168a54e972cb
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2021-10-14 12:32:46 -0400

    Update main.swift

--- Spotlight-RecentFilesCheck-Swift/Sources/Spotlight-RecentFiles-Swift/main.swift
+++ Spotlight-RecentFilesCheck-Swift/Sources/Spotlight-RecentFiles-Swift/main.swift
@@ -9,9 +9,11 @@ var p1 = 0
 var p2 = 0
 var p3 = 0
 let fileMan = FileManager.default
+var c1 = 0
+var c2 = 0
 
-print("##########################Recently Changed Files Check###############################")
-let queryString = ##"kMDItemFSName="*.*" && kMDItemFSContentChangeDate >= $time.this_week(-2)"##
+print("##########################Recently Created Files Check###############################")
+let queryString = ##"kMDItemFSName="*.*" && kMDItemFSCreationDate >= $time.this_week(-2)"##
 if let query = MDQueryCreate(kCFAllocatorDefault, queryString as CFString, nil, nil) {
     MDQueryExecute(query, CFOptionFlags(kMDQuerySynchronous.rawValue))
 
@@ -19,9 +21,13 @@ if let query = MDQueryCreate(kCFAllocatorDefault, queryString as CFString, nil, 
         if let rawPtr = MDQueryGetResultAtIndex(query, i) {
             let item = Unmanaged<MDItem>.fromOpaque(rawPtr).takeUnretainedValue()
             if let path = MDItemCopyAttribute(item, kMDItemPath) as? String {
-                if (path.contains("/Users/\(username)")) && !(path.contains("/Application Support/")) && !(path.contains("/Containers/")) && !(path.contains("/Library/")) && !(path.contains("/Movies/")) && !(path.contains("Homebrew")) && !(path.contains("Xcode")) && !(path.contains("/Pictures")) {
-                    print(path)
-                    
+
+                if !(path.contains("/Application Support/")) && !(path.contains("/Library")){
+                    c1 = c1 + 1
+                    if c1 <= 50 {
+                        print(path)
+                    }
+
                 }
                 
                 
@@ -35,7 +41,7 @@ print("-------------------------------------------------------------------------
 
 
 
-print("\n##########################Recently Created Files Check###############################")
+print("\n##########################Recently Modified Files Check###############################")
 let queryString2 = ##"kMDItemFSName="*.*" && kMDItemFSContentChangeDate >= $time.this_week(-2)"##
 if let query2 = MDQueryCreate(kCFAllocatorDefault, queryString2 as CFString, nil, nil) {
     MDQueryExecute(query2, CFOptionFlags(kMDQuerySynchronous.rawValue))
@@ -45,7 +51,12 @@ if let query2 = MDQueryCreate(kCFAllocatorDefault, queryString2 as CFString, nil
             let item2 = Unmanaged<MDItem>.fromOpaque(rawPtr2).takeUnretainedValue()
             if let path2 = MDItemCopyAttribute(item2, kMDItemPath) as? String {
                 if (path2.contains("/Users/\(username)")) && !(path2.contains("/Application Support/")) && !(path2.contains("/Containers/")) && !(path2.contains("/Library/")) && !(path2.contains("/Movies/")) && !(path2.contains("Homebrew")) && !(path2.contains("Xcode")) && !(path2.contains("/Pictures")) {
-                    print(path2)
+                    c2 = c2 + 1
+                    if c2 <= 50 {
+                        print(path2)
+                        
+                    }
+                    
                     
                 }
                 
